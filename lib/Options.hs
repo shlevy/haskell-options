91602ff369aa564834969204e7d8d4706a96a709
John Millikin <jmillikin@gmail.com>
2012-03-17 11:26:48 -0700

    Add more option types.

--- lib/Options.hs
+++ lib/Options.hs
@@ -59,8 +59,15 @@ module Options
 	, defineOptions
 	
 	-- ** Simple option definitions
-	, stringOption
 	, boolOption
+	, stringOption
+	, stringsOption
+	, textOption
+	, textsOption
+	, intOption
+	, integerOption
+	, floatOption
+	, doubleOption
 	
 	-- ** Using imported options
 	, options
@@ -78,10 +85,31 @@ module Options
 	
 	-- ** Option types
 	, OptionType
-	, optionTypeString
+	
 	, optionTypeBool
-	, optionTypeEnum
+	
+	, optionTypeString
+	, optionTypeText
+	
+	, optionTypeInt
+	, optionTypeInt8
+	, optionTypeInt16
+	, optionTypeInt32
+	, optionTypeInt64
+	, optionTypeWord
+	, optionTypeWord8
+	, optionTypeWord16
+	, optionTypeWord32
+	, optionTypeWord64
+	, optionTypeInteger
+	
+	, optionTypeFloat
+	, optionTypeDouble
+	
 	, optionTypeList
+	, optionTypeSet
+	, optionTypeMap
+	, optionTypeEnum
 	
 	-- * Option groups
 	, Group
@@ -98,7 +126,12 @@ module Options
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader
 import           Control.Monad.Writer
-import           Data.List (foldl')
+import           Data.Int
+import           Data.List (foldl', intercalate)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Text as Text
+import           Data.Word
 import qualified System.Environment
 import           System.Exit (exitFailure, exitSuccess)
 import           System.IO
@@ -125,13 +158,8 @@ class Options a where
 -- available, covering most basic types and a few more advanced types.
 data OptionType a = OptionType Type Bool (Q Exp)
 
--- | Store an option in a String. The option is stored as-is, with no further
--- transformations.
-optionTypeString :: OptionType String
-optionTypeString = OptionType (ConT ''String) False [| Right |]
-
--- | Store an option in a Bool. The option's value must be either @\"true\"@
--- or @\"false\"@.
+-- | Store an option in a @'Bool'@. The option's value must be either
+-- @\"true\"@ or @\"false\"@.
 --
 -- Boolean options are unary, which means that their value is optional when
 -- specified on the command line. If a flag is present, the option is set to
@@ -156,51 +184,170 @@ optionTypeBool = OptionType (ConT ''Bool) True [| \s -> case s of
 	-- TODO: include option flag
 	_ -> Left ("invalid boolean value: " ++ show s) |]
 
--- | Store an option as one of a set of enumerated constructors. The option
--- type must be defined in a separate file, otherwise the compiler will be
--- unable to use it when defining options.
---
--- >-- Types.hs
--- >data Mode = ModeFoo | ModeBar
--- >
--- >optionTypeMode :: OptionType Mode
--- >optionTypeMode = optionTypeEnum ''Mode
--- >    [ ("foo", 'ModeFoo)
--- >    , ("bar", 'ModeBar)
--- >    ]
---
--- >-- Main.hs
--- >import Types
--- >
--- >defineOptions "MainOptions" $ do
--- >    option "optMode" (\o -> o
--- >        { optionLongFlags = ["mode"]
--- >        , optionDefault = "foo"
--- >        , optionType = optionTypeMode
--- >        })
-optionTypeEnum :: Name -> [(String, Name)] -> OptionType a
-optionTypeEnum typeName values =
-	-- TODO: check whether vName is a valid constructor name, and use either ConE or VarE
-	let qExprs = return (ListE [TupE [LitE (StringL key), ConE vName] | (key, vName) <- values]) in
-	OptionType (ConT typeName) False
-		[| let exprs = $qExprs in \s -> case lookup s exprs of
-			Just v -> Right v
-			-- TODO: include option flag and available values
-			Nothing -> Left ("invalid enum value: " ++ show s) |]
+-- | Store an option in a @'String'@. The option is stored as-is, with no
+-- further transformations.
+optionTypeString :: OptionType String
+optionTypeString = OptionType (ConT ''String) False [| Right |]
 
--- | Store an option as a list, using another option type as the elements.
+-- | Store an option in a @'Text'@. The option is stored as-is, with no further
+-- transformations.
+optionTypeText :: OptionType Text.Text
+optionTypeText = OptionType (ConT ''Text.Text) False [| Right . Text.pack |]
+
+parseInteger :: String -> String -> Either String Integer
+parseInteger label s = parsed where
+	parsed = if valid
+		then Right (read s)
+		else Left ("invalid " ++ label ++ ": " ++ show s)
+	valid = case s of
+		[] -> False
+		'-':s' -> allDigits s'
+		_ -> allDigits s
+	allDigits = all (\c -> c >= '0' && c <= '9')
+
+parseBoundedIntegral :: (Bounded a, Integral a) => String -> String -> Either String a
+parseBoundedIntegral label = parse where
+	getBounds :: (Bounded a, Integral a) => (String -> Either String a) -> a -> a -> (Integer, Integer)
+	getBounds _ min' max' = (toInteger min', toInteger max')
+	
+	(minInt, maxInt) = getBounds parse minBound maxBound
+	
+	parse s = case parseInteger label s of
+		Left err -> Left err
+		Right int -> if minInt <= int && int <= maxInt
+			then Right (fromInteger int)
+			else Left ("invalid " ++ label ++ ": " ++ show s)
+
+parseFloat :: Read a => String -> String -> Either String a
+parseFloat label s = case reads s of
+	[(x, "")] -> Right x
+	_ -> Left ("invalid " ++ label ++ ": " ++ show s)
+
+-- | Store an option in an @'Int'@. The option value must be an integer /n/
+-- such that @'minBound' <= n <= 'maxBound'@.
+optionTypeInt :: OptionType Int
+optionTypeInt = OptionType (ConT ''Int) False [| parseBoundedIntegral "int" |]
+
+-- | Store an option in an @'Int8'@. The option value must be an integer /n/
+-- such that @'minBound' <= n <= 'maxBound'@.
+optionTypeInt8 :: OptionType Int8
+optionTypeInt8 = OptionType (ConT ''Int8) False [| parseBoundedIntegral "int8" |]
+
+-- | Store an option in an @'Int16'@. The option value must be an integer /n/
+-- such that @'minBound' <= n <= 'maxBound'@.
+optionTypeInt16 :: OptionType Int16
+optionTypeInt16 = OptionType (ConT ''Int16) False [| parseBoundedIntegral "int16" |]
+
+-- | Store an option in an @'Int32'@. The option value must be an integer /n/
+-- such that @'minBound' <= n <= 'maxBound'@.
+optionTypeInt32 :: OptionType Int32
+optionTypeInt32 = OptionType (ConT ''Int32) False [| parseBoundedIntegral "int32" |]
+
+-- | Store an option in an @'Int64'@. The option value must be an integer /n/
+-- such that @'minBound' <= n <= 'maxBound'@.
+optionTypeInt64 :: OptionType Int64
+optionTypeInt64 = OptionType (ConT ''Int64) False [| parseBoundedIntegral "int64" |]
+
+-- | Store an option in a @'Word'@. The option value must be a positive
+-- integer /n/ such that @0 <= n <= 'maxBound'@.
+optionTypeWord :: OptionType Word
+optionTypeWord = OptionType (ConT ''Word) False [| parseBoundedIntegral "word" |]
+
+-- | Store an option in a @'Word8'@. The option value must be a positive
+-- integer /n/ such that @0 <= n <= 'maxBound'@.
+optionTypeWord8 :: OptionType Word8
+optionTypeWord8 = OptionType (ConT ''Word8) False [| parseBoundedIntegral "word8" |]
+
+-- | Store an option in a @'Word16'@. The option value must be a positive
+-- integer /n/ such that @0 <= n <= 'maxBound'@.
+optionTypeWord16 :: OptionType Word16
+optionTypeWord16 = OptionType (ConT ''Word16) False [| parseBoundedIntegral "word16" |]
+
+-- | Store an option in a @'Word32'@. The option value must be a positive
+-- integer /n/ such that @0 <= n <= 'maxBound'@.
+optionTypeWord32 :: OptionType Word32
+optionTypeWord32 = OptionType (ConT ''Word32) False [| parseBoundedIntegral "word32" |]
+
+-- | Store an option in a @'Word64'@. The option value must be a positive
+-- integer /n/ such that @0 <= n <= 'maxBound'@.
+optionTypeWord64 :: OptionType Word64
+optionTypeWord64 = OptionType (ConT ''Word64) False [| parseBoundedIntegral "word64" |]
+
+-- | Store an option in an @'Integer'@. The option value must be an integer.
+-- There is no minimum or maximum value.
+optionTypeInteger :: OptionType Integer
+optionTypeInteger = OptionType (ConT ''Integer) False [| parseInteger "integer" |]
+
+-- | Store an option in a @'Float'@. The option value must be a number. Due to
+-- the imprecision of floating-point math, the stored value might not exactly
+-- match the user's input. If the user's input is out of range for the
+-- @'Float'@ type, it will be stored as @Infinity@ or @-Infinity@.
+optionTypeFloat :: OptionType Float
+optionTypeFloat = OptionType (ConT ''Float) False [| parseFloat "float" |]
+
+-- | Store an option in a @'Double'@. The option value must be a number. Due to
+-- the imprecision of floating-point math, the stored value might not exactly
+-- match the user's input. If the user's input is out of range for the
+-- @'Double'@ type, it will be stored as @Infinity@ or @-Infinity@.
+optionTypeDouble :: OptionType Double
+optionTypeDouble = OptionType (ConT ''Double) False [| parseFloat "double" |]
+
+-- | Store an option as a list, using another option type for the elements.
 -- The separator should be a character that will not occur within the values,
 -- such as a comma or semicolon.
 --
--- >    option "optNames" (\o -> o
--- >        { optionLongFlags = ["names"]
--- >        , optionDefault = "Alice;Bob;Charles"
--- >        , optionType = optionTypeList ';' optionTypeString
--- >        })
-optionTypeList :: Char -- ^ Separator
+-- >option "optNames" (\o -> o
+-- >    { optionLongFlags = ["names"]
+-- >    , optionDefault = "Alice;Bob;Charles"
+-- >    , optionType = optionTypeList ';' optionTypeString
+-- >    })
+optionTypeList :: Char -- ^ Element separator
                -> OptionType a -- ^ Element type
                -> OptionType [a]
 optionTypeList sep (OptionType valType _ valParseExp) = OptionType (AppT ListT valType) False [| \s -> parseList $valParseExp (split sep s) |]
+
+-- | Store an option as a @'Set.Set'@, using another option type for the
+-- elements. The separator should be a character that will not occur within
+-- the values, such as a comma or semicolon.
+--
+-- Duplicate elements in the input are permitted.
+--
+-- >option "optNames" (\o -> o
+-- >    { optionLongFlags = ["names"]
+-- >    , optionDefault = "Alice;Bob;Charles"
+-- >    , optionType = optionTypeSet ';' optionTypeString
+-- >    })
+optionTypeSet :: Ord a
+              => Char -- ^ Element separator
+              -> OptionType a -- ^ Element type
+              -> OptionType (Set.Set a)
+optionTypeSet sep (OptionType valType _ valParseExp) = OptionType (AppT (ConT ''Set.Set) valType) False [| \s -> parseSet $valParseExp (split sep s) |]
+
+-- | Store an option as a 'Map.Map', using other option types for the keys and
+-- values.
+--
+-- The item separator is used to separate key/value pairs from eachother. It
+-- should be a character that will not occur within either the keys or values.
+--
+-- The value separator is used to separate the key from the value. It should
+-- be a character that will not occur within the keys. It may occur within the
+-- values.
+--
+-- Duplicate keys in the input are permitted. The final value for each key is
+-- stored.
+--
+-- >option "optNames" (\o -> o
+-- >    { optionLongFlags = ["names"]
+-- >    , optionDefault = "name=Alice;hometown=Bucharest"
+-- >    , optionType = optionTypeMap ';' '=' optionTypeString optionTypeString
+-- >    })
+optionTypeMap :: Ord k
+              => Char -- ^ Item separator
+              -> Char -- ^ Key/Value separator
+              -> OptionType k -- ^ Key type
+              -> OptionType v -- ^ Value type
+              -> OptionType (Map.Map k v)
+optionTypeMap itemSep keySep (OptionType keyType _ keyParseExp) (OptionType valType _ valParseExp) = OptionType (AppT (AppT (ConT ''Map.Map) keyType) valType) False [| \s -> parseMap keySep $keyParseExp $valParseExp (split itemSep s) |]
 
 parseList :: (String -> Either String a) -> [String] -> Either String [a]
 parseList p = loop where
@@ -211,6 +358,25 @@ parseList p = loop where
 			Left err -> Left err
 			Right vs -> Right (v:vs)
 
+parseSet :: Ord a => (String -> Either String a) -> [String] -> Either String (Set.Set a)
+parseSet p strs = case parseList p strs of
+	Left err -> Left err
+	Right xs -> Right (Set.fromList xs)
+
+parseMap :: Ord k => Char -> (String -> Either String k) -> (String -> Either String v) -> [String] -> Either String (Map.Map k v)
+parseMap keySep pKey pVal = parsed where
+	parsed strs = case parseList pItem strs of
+		Left err -> Left err
+		Right xs -> Right (Map.fromList xs)
+	pItem s = case break (== keySep) s of
+		(sKey, valAndSep) -> case valAndSep of
+			[] -> Left ("invalid map item with no value: " ++ show s)
+			_ : sVal -> case pKey sKey of
+				Left err -> Left err
+				Right key -> case pVal sVal of
+					Left err -> Left err
+					Right val -> Right (key, val)
+
 split :: Char -> String -> [String]
 split _ [] = []
 split sep s0 = loop s0 where
@@ -218,6 +384,35 @@ split sep s0 = loop s0 where
 		(chunk, rest) = break (== sep) s
 		cont = chunk : loop (tail rest)
 		in if null rest then [chunk] else cont
+
+-- | Store an option as one of a set of enumerated constructors. The option
+-- type must be defined in a separate file, otherwise the compiler will be
+-- unable to use it when defining options.
+--
+-- >-- MyApp.Types.hs
+-- >data Mode = ModeFoo | ModeBar
+--
+-- >-- Main.hs
+-- >import MyApp.Types
+-- >
+-- >defineOptions "MainOptions" $ do
+-- >    option "optMode" (\o -> o
+-- >        { optionLongFlags = ["mode"]
+-- >        , optionDefault = "foo"
+-- >        , optionType = optionTypeEnum ''Mode
+-- >            [ ("foo", 'ModeFoo)
+-- >            , ("bar", 'ModeBar)
+-- >            ]
+-- >        })
+optionTypeEnum :: Name -> [(String, Name)] -> OptionType a
+optionTypeEnum typeName values =
+	-- TODO: check whether vName is a valid constructor name, and use either ConE or VarE
+	let qExprs = return (ListE [TupE [LitE (StringL key), ConE vName] | (key, vName) <- values]) in
+	OptionType (ConT typeName) False
+		[| let exprs = $qExprs in \s -> case lookup s exprs of
+			Just v -> Right v
+			-- TODO: include option flag and available values
+			Nothing -> Left ("invalid enum value: " ++ show s) |]
 
 -- | See 'option'.
 data Option a = Option
@@ -362,7 +557,8 @@ putOptionDecl name qtype infoExp parseExp = OptionsM (tell [(name, qtype, infoEx
 --
 -- >option "optFoo" (\o -> o
 -- >    { optionLongFlags = ["names"]
--- >    , optionType = optionTypeList ',' optionTypeString
+-- >    , optionDefault = "Alice;Bob;Charles"
+-- >    , optionType = optionTypeList ';' optionTypeString
 -- >    }
 option :: String -- ^ Field name
        -> (Option String -> Option a) -- ^ Option definition
@@ -485,7 +681,21 @@ suboptsDefs rec = defsB where
 	defsA :: Options a => (b -> a) -> OptionDefinitions a
 	defsA _ = optionsDefs
 
--- | Define an option of type @String@. This is a simple wrapper around
+-- | Define an option of type @'Bool'@. This is a simple wrapper around
+-- 'option'.
+boolOption :: String -- ^ Field name
+           -> String -- ^ Long flag
+           -> Bool -- ^ Default value
+           -> String -- ^ Description in @--help@
+           -> OptionsM ()
+boolOption name flag def desc = option name (\o -> o
+	{ optionLongFlags = [flag]
+	, optionDefault = if def then "true" else "false"
+	, optionType = optionTypeBool
+	, optionDescription = desc
+	})
+
+-- | Define an option of type @'String'@. This is a simple wrapper around
 -- 'option'.
 stringOption :: String -- ^ Field name
              -> String -- ^ Long flag
@@ -498,17 +708,100 @@ stringOption name flag def desc = option name (\o -> o
 	, optionDescription = desc
 	})
 
--- | Define an option of type @Bool@. This is a simple wrapper around
+-- | Define an option of type @['String']@. This is a simple wrapper around
+-- 'option'. Items are comma-separated.
+stringsOption :: String -- ^ Field name
+              -> String -- ^ Long flag
+              -> [String] -- ^ Default value
+              -> String -- ^ Description in @--help@
+              -> OptionsM ()
+stringsOption name flag def desc = option name (\o -> o
+	{ optionLongFlags = [flag]
+	, optionDefault = intercalate "," def
+	, optionType = optionTypeList ',' optionTypeString
+	, optionDescription = desc
+	})
+
+-- | Define an option of type @'Text.Text'@. This is a simple wrapper around
 -- 'option'.
-boolOption :: String -- ^ Field name
+textOption :: String -- ^ Field name
            -> String -- ^ Long flag
-           -> Bool -- ^ Default value
+           -> Text.Text -- ^ Default value
            -> String -- ^ Description in @--help@
            -> OptionsM ()
-boolOption name flag def desc = option name (\o -> o
+textOption name flag def desc = option name (\o -> o
 	{ optionLongFlags = [flag]
-	, optionDefault = if def then "true" else "false"
-	, optionType = optionTypeBool
+	, optionDefault = Text.unpack def
+	, optionDescription = desc
+	})
+
+-- | Define an option of type @['Text.Text']@. This is a simple wrapper around
+-- 'option'. Items are comma-separated.
+textsOption :: String -- ^ Field name
+            -> String -- ^ Long flag
+            -> [Text.Text] -- ^ Default value
+            -> String -- ^ Description in @--help@
+            -> OptionsM ()
+textsOption name flag def desc = option name (\o -> o
+	{ optionLongFlags = [flag]
+	, optionDefault = Text.unpack (Text.intercalate (Text.pack ",") def)
+	, optionType = optionTypeList ',' optionTypeText
+	, optionDescription = desc
+	})
+
+-- | Define an option of type @'Int'@. This is a simple wrapper around
+-- 'option'.
+intOption :: String -- ^ Field name
+          -> String -- ^ Long flag
+          -> Int -- ^ Default value
+          -> String -- ^ Description in @--help@
+          -> OptionsM ()
+intOption name flag def desc = option name (\o -> o
+	{ optionLongFlags = [flag]
+	, optionDefault = show def
+	, optionType = optionTypeInt
+	, optionDescription = desc
+	})
+
+-- | Define an option of type @'Integer'@. This is a simple wrapper around
+-- 'option'.
+integerOption :: String -- ^ Field name
+              -> String -- ^ Long flag
+              -> Integer -- ^ Default value
+              -> String -- ^ Description in @--help@
+              -> OptionsM ()
+integerOption name flag def desc = option name (\o -> o
+	{ optionLongFlags = [flag]
+	, optionDefault = show def
+	, optionType = optionTypeInteger
+	, optionDescription = desc
+	})
+
+-- | Define an option of type @'Float'@. This is a simple wrapper around
+-- 'option'.
+floatOption :: String -- ^ Field name
+            -> String -- ^ Long flag
+            -> Float -- ^ Default value
+            -> String -- ^ Description in @--help@
+            -> OptionsM ()
+floatOption name flag def desc = option name (\o -> o
+	{ optionLongFlags = [flag]
+	, optionDefault = show def
+	, optionType = optionTypeFloat
+	, optionDescription = desc
+	})
+
+-- | Define an option of type @'Double'@. This is a simple wrapper around
+-- 'option'.
+doubleOption :: String -- ^ Field name
+             -> String -- ^ Long flag
+             -> Double -- ^ Default value
+             -> String -- ^ Description in @--help@
+             -> OptionsM ()
+doubleOption name flag def desc = option name (\o -> o
+	{ optionLongFlags = [flag]
+	, optionDefault = show def
+	, optionType = optionTypeDouble
 	, optionDescription = desc
 	})
 
